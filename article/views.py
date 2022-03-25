b0ef49ce392e192e7284830b38792bd9a05f9c50
keevinzha <keevinzha@163.com>
2022-03-26 01:35:33 +0800

    增加了markdown语法支持和代码高亮

--- article/views.py
+++ article/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ArticlePost
+import markdown
 # Create your views here.
 
 def article_list(request):
@@ -12,5 +13,10 @@ def article_list(request):
 
 def article_detail(request, id):
     article = ArticlePost.objects.get(id=id)
+    #   渲染markdown语法
+    article.body = markdown.markdown(article.body, extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+    ])
     context = {'article':article}
     return render(request, 'article/detail.html', context)
